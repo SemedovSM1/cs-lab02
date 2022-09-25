4333814c0d2dc3104b895e5d021c97b2341a6e83
Bob (SemedovSM) <SemedovSM@mpei.ru>
2022-09-25 09:55:25 +0300

    code: вывод произведения

--- main.cpp
+++ main.cpp
@@ -11,7 +11,9 @@ int main()
 
     cout << "A + B = " << a + b << '\n'
 
-         << "A - B = " << a - b << '\n';
+         << "A - B = " << a - b << '\n'
+
+         << "A * B = " << a * b << '\n';
 
     return 0;
 }
